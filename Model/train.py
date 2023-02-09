561623285beb9455a14eee14011987c47b0e300d
fullzer4 <gabrielpelizzaro@gmail.com>
2023-02-09 00:26:41 -0300

    new

--- Model/train.py
+++ Model/train.py
@@ -8,6 +8,10 @@ import subprocess
 import os
 from dotenv import load_dotenv
 
+HOME = os.getcwd()
+
+print(HOME)
+
 load_dotenv()
 
 apikey = os.getenv("api_key")
@@ -17,6 +21,8 @@ parser = argparse.ArgumentParser(description='Descrição')
 parser.add_argument('-gpu', action='store_true', help='Run model in GPU')
 parser.add_argument('-cpu', action='store_true', help='Run model in CPU')
 parser.add_argument('-train', action='store_true', help='Run model train and import dataset')
+parser.add_argument('-data', action='store_true', help='Run model train and import dataset')
+
 
 args = parser.parse_args()
 
@@ -27,26 +33,20 @@ class Starter:
             print('Error: both -gpu and -cpu cannot be used together')
             exit(1)
         elif args.gpu:
-            self.gpu_start()
+            modeLine = ['yolo', 'checks']
+            mode = subprocess.run(modeLine, capture_output=True, text=True)
+            print(mode)
+            if(torch.cuda.is_available()):
+                print('Running GPU...')
         elif args.cpu:
-            self.cpu_start()
+            import os
+            os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
         else:
             print('use -cpu or -gpu to run')
-
-    def gpu_start(self):
-        if(torch.cuda.is_available()):
-            device = torch.device("cuda")
-            print('Running GPU...')
-        else:
-            print('Running CPU because u dont have cuda install...')
-
-    def cpu_start(self):
-        device = torch.device("cpu")
-        print('Running CPU...')
         
-class train:
+class data:
     def __init__(self):
-        if args.train:
+        if args.data:
             
             display.clear_output()
             
@@ -55,13 +55,19 @@ class train:
             print(mode)    
             
             rf = Roboflow(api_key=apikey)
-            project = rf.workspace("kgx-tgvum").project("traffic-flow-jgib7")
-            self.dataset = project.version(2).download("yolov8")
-            data_path = os.path.join(self.dataset.location, "data.yaml")
-            trainLine = ['yolo', 'task=detect', 'mode=train', 'model=yolov8s.pt', f'data={data_path}', 'epochs=10', 'imgsz=256']
-            train = subprocess.run(trainLine, capture_output=True, text=True)      
-            print(train) 
-        
+            project = rf.workspace("roboflow-100").project("vehicles-q0x2v")
+            dataset = project.version(2).download("yolov8")
+            
+class train:
+    def __init__(self):
+        if args.train:
+            torch.set_default_tensor_type('torch.FloatTensor')
+            model = YOLO("yolov8s.pt")
+            model.train(data="./vehicles-2/data.yaml", epochs=10)
+            model.val()
+            
+            
 if __name__ == '__main__':
     starter = Starter()
+    data = data()
     train = train()

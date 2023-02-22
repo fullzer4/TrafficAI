b01c48be1b323d542e7fee554ad6551e8a0041f1
fullzer4 <gabrielpelizzaro@gmail.com>
2023-02-21 22:05:11 -0300

    new

---
+++ app/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import Navbar from "./components/navbar";
+
+function App() {
+
+  return (
+    <div className="container">
+      <h1>DASDHI</h1>
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ app/src/components/navbar.tsx
@@ -0,0 +1,10 @@
+function Navbar() {
+
+    return (
+      <div className="container">
+        <h1>DASDHI</h1>
+      </div>
+    );
+  }
+  
+  export default Navbar;

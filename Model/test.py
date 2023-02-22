9f92e13f55d9256e1a8d68e1b75be9cf503b6260
fullzer4 <gabrielpelizzaro@gmail.com>
2023-02-21 21:16:36 -0300

    new

--- Model/test.py
+++
@@ -1,5 +0,0 @@
-from ultralytics import YOLO
-from ultralytics.yolo.v8.detect.predict import DetectionPredictor
-
-model = YOLO("runs/detect/train7/weights/best.pt")
-model.predict(source="MVI_2378.MOV", show=True, conf=0.3)

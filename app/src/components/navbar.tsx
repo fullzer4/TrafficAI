7d423aafd180df40031bf4ae00397a2c478622c5
fullzer4 <gabrielpelizzaro@gmail.com>
2023-02-24 11:31:52 -0300

    new

---
+++ app/src/components/navbar.tsx
@@ -0,0 +1,10 @@
+import style from "../scss/navbar.module.scss"
+
+export default function Navbar(){
+    
+    return(
+        <div className={style.navbar}>
+
+        </div>
+    )
+}
--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from "next/app";
 
-import "../style.css";
-import "../App.css";
+import "../scss/reset.scss"
+import "../scss/default.scss"
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }: AppProps) {

c83daf3d573d0176f2fad06de99b1bb258b37cda
fullzer4 <gabrielpelizzaro@gmail.com>
2023-02-24 06:00:26 -0300

    new

--- Model/script.py
+++
@@ -1,7 +0,0 @@
-from roboflow import Roboflow
-import ultralytics
-import yolox
-
-rf = Roboflow(api_key="YG32Zd9Kcmc9muEmR5G4")
-project = rf.workspace("roboflow-100").project("vehicles-q0x2v")
-dataset = project.version(2).download("yolov8")

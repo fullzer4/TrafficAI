d6e2f32461d52604307149781d8c602b586274b0
fullzer4 <gabrielpelizzaro@gmail.com>
2023-02-24 12:31:33 -0300

    new

---
+++ app/src/components/dropbox.tsx
@@ -0,0 +1,10 @@
+import style from "../scss/drop.module.scss"
+
+export default function Dropbox(){
+    
+    return(
+        <div className={style.drop}>
+            <h1>teste</h1>
+        </div>
+    )
+}
--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -2,12 +2,16 @@ import { useState } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
 import Image from "next/image";
 import Navbar from "../components/navbar";
+import Dropbox from "../components/dropbox";
 
 export default function App() {
 
   return (
-    <div className="container">
+    <>
       <Navbar/>
-    </div>
+      <div className="content">
+        <Dropbox/>
+      </div>
+    </>
   );
 }
